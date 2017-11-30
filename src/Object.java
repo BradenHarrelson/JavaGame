4a66493f07730bff47b4613e58f0071eb48d1d2c
alikhalid <ali.kahn92@gmail.com>
2017-11-30 13:05:09 -0600

    made changes to object class

--- src/Object.java
+++ src/Object.java
@@ -1,4 +1,4 @@
-i//This is a generalized object class that the spaceship and asteroids will
+//This is a generalized object class that the spaceship and asteroids will
 //inherit from and extend the functionality. Both the sub classes will have
 //different move operations....
 
@@ -6,61 +6,61 @@ import javax.swing.ImageIcon;
 import java.awt.Image;
 import java.awt.Rectangle;
 
-public class Object {
+public class Objects {
 
-  //x and y coordinate represent the top left corner of the image
-  private Integer x;
-  private Integer y;
-  private Integer width;
-  private Integer height;
-  private boolean valid; //used to check if object is still on the screen
-  private Image object;
+    //x and y coordinate represent the top left corner of the image
+    protected Integer x;
+    protected Integer y;
+    private Integer width;
+    private Integer height;
+    private boolean valid; //used to check if object is still on the screen
+    protected Image object;
 
-  public Object(int x, int y) {
-    this.x = x;
-    this.y = y;
-  }
+    public Objects(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-  //objectName will be the name of a picture stored in teh folder
-  //passed in by the subclass spaceship or asteroid. (PNG, JPEG or GIF only)
-  private void loadObject(String objectName){
-    ImageIcon objectImage = new ImageIcon(objectName);
-    object = objectImage.getImage();
-  }
+    //objectName will be the name of a picture stored in the folder
+    //passed in by the subclass spaceship or asteroid. (PNG, JPEG or GIF only)
+    protected void loadObject(String objectName){
+        ImageIcon objectImage = new ImageIcon(objectName);
+        object = objectImage.getImage();
+    }
 
-  //function can not be called until the object has been loaded
-  private void extractDimensions() {
-    width = object.getWidth(null);
-    height = object.getHeight(null);
-  }
+    //function can not be called until the object has been loaded
+    protected void extractDimensions() {
+        width = object.getWidth(null);
+        height = object.getHeight(null);
+    }
 
-  public Integer getWidth() {
-    return width;
-  }
+    public Integer getX() {
+        return x;
+    }
 
-  public Integer getHeight() {
-    return height;
-  }
+    public Integer getY() {
+        return y;
+    }
 
-  public boolean isValid() {
-    return valid;
-  }
+    public boolean isValid() {
+        return valid;
+    }
 
-  public void setValid(boolean valid) {
-    this.valid = valid;
-  }
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
 
-  //object is required every time we want to redraw
-  public Image getObject() {
-    return object;
-  }
+    //object is required every time we want to redraw
+    public Image getObject() {
+        return object;
+    }
 
-  //we will need this to check for collision
-  //this function can not be called untill the object
-  // calls the extractDimensions function
-  //rectangle.intersect(rectangle) to check for collision to be
-  //defined in board class
-  public Rectangle getBoundingBox() {
-    return new Rectangle(x, y, width, height);
-  }
+    //we will need this to check for collision
+    //this function can not be called until the object
+    // calls the extractDimensions function
+    //rectangle.intersect(rectangle) to check for collision to be
+    //defined in board class
+    public Rectangle getBoundingBox() {
+        return new Rectangle(x, y, width, height);
+    }
 }

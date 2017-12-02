207eadd5dedf7d139f3bab5724dd53c40dd4863e
alikhalid <ali.kahn92@gmail.com>
2017-12-01 19:07:15 -0600

    Working BUttons. Play and quit.

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,26 +1,63 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GameRunner extends JFrame{
+
+    private JList mylist;
+    private DefaultListModel model;
 
     public GameRunner() {
         initGame();
     }
 
     private void initGame() {
-        JFrame spaceFrame = new JFrame("Space");
-        JFrame mainFrame = new JFrame("Main Screen");
 
-        spaceFrame.add(new MainMenu());
+        JPanel panel = new JPanel();
 
-        spaceFrame.add(new Space());
+        JButton start_game = new JButton("Play");
+        JButton quit = new JButton("Quit");;
 
-        spaceFrame.setResizable(true);
-        spaceFrame.pack();
-        spaceFrame.setTitle("Spaceship Game");
-        spaceFrame.setLocationRelativeTo(null);
-        spaceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        spaceFrame.setVisible(true);
+        setTitle("Asteroid");
+        setSize(200, 200);
+        panel.setBackground(Color.black);
+
+        panel.add(start_game);
+        panel.add(quit);
+        // And JPanel needs to be added to the JFrame itself!
+        this.getContentPane().add(panel);
+
+        start_game.setBackground(Color.green);
+        quit.setBackground(Color.red);
+
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        start_game.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource() instanceof JButton) {
+                    setVisible(false);
+                    JFrame spaceFrame = new JFrame("Space");
+                    spaceFrame.add(new Space());
+                    spaceFrame.setResizable(true);
+                    spaceFrame.pack();
+                    spaceFrame.setTitle("Asteroid");
+                    spaceFrame.setLocationRelativeTo(null);
+                    spaceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    spaceFrame.setVisible(true);
+                }
+            }
+        });
+
+        quit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
     }
 
     public static void main(String[] args){
